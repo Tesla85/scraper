d53d9d17db5a4f02702127d90ae6e6fd23a644e0
Brandt <brandt.strait@gmail.com>
2018-04-19 20:41:36 -0400

    update

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ var bodyparser = require("body-parser");
 var cheerio = require("cheerio");
 var request = require("request");
 
+var PORT = process.env.PORT || 3000;
 // Requiring the `Example` model for accessing the `examples` collection
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper-brandt";
 
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/schemaexample");
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
